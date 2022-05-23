00c05b647109500c4a366a68af9b7230b1f5046d
Arisa Hirata <arisa.pirata@gmail.com>
2022-05-23 13:34:05 -0700

    Using React context and the useState hook

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FiShoppingCart } from 'react-icons/fi';
 import CartCSS from './Cart.module.css';
+import { AppStateContext } from './AppState';
 
 interface Props { }
 
@@ -16,31 +17,38 @@ class Cart extends React.Component<Props, State> {
     };
   }
 
+  handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
+  }
+
   render() {
     return (
-      <div className={CartCSS.cartContainer}>
-        <button
-          className={CartCSS.button}
-          type="button"
-          onClick={() => {
-            this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
-          }}
-        >
-          <FiShoppingCart />
-          <span>2 pizza(s)</span>
-        </button>
-        <div
-          className={CartCSS.cartDropDown}
-          style={{
-            display: this.state.isOpen ? 'block' : 'none'
-          }}
-        >
-          <ul>
-            <li>Napoletana</li>
-            <li>Marinara</li>
-          </ul>
-        </div>
-      </div>
+      <AppStateContext.Consumer>{(state) => {
+        return (
+          <div className={CartCSS.cartContainer}>
+            <button
+              className={CartCSS.button}
+              type="button"
+              onClick={this.handleClick}
+            >
+              <FiShoppingCart />
+              <span>{state.cart.items.length} pizza(s)</span>
+            </button>
+            <div
+              className={CartCSS.cartDropDown}
+              style={{
+                display: this.state.isOpen ? 'block' : 'none'
+              }}
+            >
+              <ul>
+                {state.cart.items.map(item => {
+                  return <li key={item.id}>{item.name}</li>
+                })}
+              </ul>
+            </div>
+          </div>
+        );
+      }}</AppStateContext.Consumer>
     );
   }
 }
