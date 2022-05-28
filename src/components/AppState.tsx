1456fe424f00f5c2488d104052d6a895002102f0
Arisa Hirata <arisa.pirata@gmail.com>
2022-05-28 10:57:15 -0700

    Creating a Higher Order Component - part 2

--- src/components/AppState.tsx
+++ src/components/AppState.tsx
@@ -1,7 +1,12 @@
 import React, { createContext, useContext, useReducer, useEffect } from 'react';
 
 
-interface CartItem { id: number; name: string; price: number; quantity: number }
+export interface CartItem {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number
+}
 interface AppStateValue {
   cart: {
     items: CartItem[];
