b82407493256ed67a78924bd473f59bb7b876ee9
Arisa Hirata <arisa.pirata@gmail.com>
2022-05-28 13:01:14 -0700

    when the cart popup is clicked the cart isn't closed

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import { FiShoppingCart } from 'react-icons/fi';
 import CartCSS from './Cart.module.css';
 import { AppStateContext } from './AppState';
@@ -10,15 +10,26 @@ interface State {
 }
 
 class Cart extends React.Component<Props, State> {
+  #containerRef: React.RefObject<HTMLDivElement>;
+
   constructor(props: Props) {
     super(props);
     this.state = {
       isOpen: false,
     };
+    this.#containerRef = createRef();
   }
 
   handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
+  };
+
+  componentDidMount() {
+    document.addEventListener('mousedown', (e) => {
+      if (this.#containerRef.current && !this.#containerRef.current.contains(e.target as Node)) {
+        this.setState({ isOpen: false });
+      } ;
+    });
   }
 
   render() {
@@ -29,7 +40,7 @@ class Cart extends React.Component<Props, State> {
             return sum + item.quantity;
           }, 0);
           return (
-            <div className={CartCSS.cartContainer}>
+            <div className={CartCSS.cartContainer} ref={this.#containerRef}>
               <button
                 className={CartCSS.button}
                 type="button"
